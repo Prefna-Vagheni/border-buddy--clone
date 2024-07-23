c28c3c8380951f73530e6f0d7131beed4686a349
Prefna Vagheni <prefnavagheni54@gmail.com>
2024-07-24 00:36:31 +0300

    Cleaning codes

---
+++ README.md
@@ -0,0 +1,8 @@
+#Description
+
+This project is a test for HTML and CSS level.
+It implies some of javascript for tiny features found on the page.
+
+##Detail - CSS related
+
+The secondary pages, the ones connected to the index.html did not be the subject of responsiveness. Since I used the mobile first approach, that means those pages did not be object of media queries. Therefore, they may seem strange on desktop screen sizes. In the future We will consider makink them fully responsive to the same level as the index.html.
--- js/script.js
+++ js/script.js
@@ -59,8 +59,6 @@ searchBtn.addEventListener('click', () => {
 //COUNTER UP
 counters.forEach((counter) => {
   counter.textContent = '0';
-  // let num = Intl.NumberFormat('en-US').format(+counter);
-  // console.log(num);
   const updateCounter = function () {
     const target = +counter.getAttribute('data-target');
     const numFormat = Intl.NumberFormat('en-US').format(target);
@@ -134,18 +132,6 @@ const start = () => {
 };
 start();
 
-// startInterval();
-// const startInterval = () => {
-//   setInterval(() => {
-//     index++;
-//     updateContent();
-//   }, speed);
-// };
-// updateContent();
-// function updateContent() {
-//   paragraphs.forEach((paragraph) => paragraph.classList.remove('active'));
-//   // paragraphs[index].classList.add('active');
-// }
 //==================FAQ PAGE====================\\
 const faqToggleBtn = document.querySelectorAll('.faq__heading');
 const hiddenParagraph = document.querySelectorAll('.faq__paragraph');
@@ -162,137 +148,3 @@ for (const key in hiddenParagraph) {
     });
   });
 }
-
-// faqToggleBtn.forEach((button) =>
-//   // for (const key in hiddenParagraph){
-//   //   button.addEventListener.
-//   // }
-//   // hiddenParagraph.forEach((paragraph) =>
-//   //   button.addEventListener('click', () => {
-//   //     // alert('hello');
-
-//   //     if (!paragraph.classList.contains('visible__paragraph')) {
-//   //       // hiddenParagraph.forEach((paragraph) =>
-//   //       // );
-//   //       paragraph.classList.remove('visible__paragraph');
-//   //       paragraph.classList.add('visible__paragraph');
-//   //       // hiddenParagraph.classList.remove('visible__paragraph');
-//   //       // hiddenParagraph.classList.add('visible__paragraph');
-//   //     } else {
-//   //       paragraph.classList.remove('visible__paragraph');
-//   //     }
-//   //   })
-//   // )
-// );
-
-// const valueDisplay = document.querySelectorAll('.counting__value');
-// const slides = document.querySelectorAll('.slide');
-// const slider = document.querySelector('.slider');
-// const dotContainer = document.querySelector('.dots');
-// const header = document.querySelector('header');
-// const year = document.getElementById('year');
-
-// let currentSlide = 0;
-// let interval = 500;
-// let slideInterval = 5000;
-
-// //Sticky nav bar
-// const stickyNav = (entries) => {
-//   const [entry] = entries;
-
-//   if (!entry.isIntersecting) header.classList.toggle('sticky__nav');
-//   // else header.classList.remove("sticky__nav");
-// };
-
-// const stickyOptions = {
-//   root: null,
-//   threshold: 0.2,
-//   // rootMargin: "-100px",
-// };
-
-// const headerObserver = new IntersectionObserver(stickyNav, stickyOptions);
-// headerObserver.observe(header);
-
-// //Dynamic footer copyright date
-// let getDate = new Date().getFullYear();
-// year.textContent = getDate;
-
-// // valueDisplay.forEach((value) => {
-// //   let start = 0;
-// //   let end = Number(value.getAttribute("data-val"));
-
-// //   let duration = Math.floor(interval / end);
-// //   let counter = setInterval(function () {
-// //     start += 1;
-// //     value.textContent = start;
-// //     if (start === end) {
-// //       clearInterval(counter);
-// //     }
-// //   }, duration);
-// // });
-
-// valueDisplay.forEach((value) => {
-//   const updateData = function () {
-//     const target = Number(value.getAttribute('data-val'));
-//     const count = Number(value.innerHTML);
-//     let inc = target / interval;
-//     if (count < target) {
-//       value.innerText = Math.floor(inc + count);
-//       setTimeout(updateData, 1);
-//     } else {
-//       value.innerText = target;
-//     }
-//   };
-//   updateData();
-// });
-
-// //Slider
-// const createDots = () => {
-//   slides.forEach((_, i) => {
-//     dotContainer.insertAdjacentHTML(
-//       'beforeend',
-//       `<button class='dots__dot' data-slide='${i}'></button>`
-//     );
-//   });
-// };
-
-// createDots();
-
-// const activeDot = function (slide) {
-//   document
-//     .querySelectorAll('.dots__dot')
-//     .forEach((dot) => dot.classList.remove('dots__dot--active'));
-//   document
-//     .querySelector(`.dots__dot[data-slide='${slide}'`)
-//     .classList.add('dots__dot--active');
-// };
-// activeDot(0);
-// const goToSlide = (slide) => {
-//   slides.forEach(
-//     (s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`)
-//   );
-// };
-// goToSlide(0);
-
-// const startSlide = () => {
-//   setInterval(() => {
-//     if (currentSlide === slides.length - 1) {
-//       currentSlide = 0;
-//     } else {
-//       currentSlide++;
-//     }
-//     slides.forEach(
-//       (s, i) => (s.style.transform = `translateX(${100 * (i - currentSlide)}%)`)
-//     );
-//     activeDot(currentSlide);
-//   }, slideInterval);
-// };
-
-// dotContainer.addEventListener('click', (e) => {
-//   if (e.target.classList.contains('dots__dot')) {
-//     const { slide } = e.target.dataset;
-//     goToSlide(slide);
-//   }
-// });
-
-// startSlide();
